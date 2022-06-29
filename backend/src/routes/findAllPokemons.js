d2a8c3f32e0f318eac6b7dcf6279fdc9281727bf
datencrypted <gamerdragoon98@gmail.com>
2022-06-29 17:03:39 +0200

    BDD qui se genere depuis mock-pokemons.js / bugs certains ne s'inscrivent pas dans la BDD

--- backend/src/routes/findAllPokemons.js
+++ backend/src/routes/findAllPokemons.js
@@ -3,6 +3,7 @@ const PokemonModel = require("../models/pokemons");
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
     PokemonModel.find()
+      .sort({ id: 1 })
       .then((pokemons) => {
         res.json(pokemons);
       })
